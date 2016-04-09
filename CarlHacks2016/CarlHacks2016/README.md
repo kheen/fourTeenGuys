6384e884f5178cd40b72852b877f6a30aa566afb
kheen <kheen@macalester.edu>
2016-04-08 22:15:10 -0500

    Added files via upload

---
+++ CarlHacks2016/CarlHacks2016/README.md
@@ -0,0 +1,2 @@
+# fourTeenGuys
+CarlHacks
---
+++ CarlHacks2016/TestModule/src/testPackage/TestClass.java
@@ -0,0 +1,12 @@
+package testPackage;
+
+/**
+ * Created by Kai on 4/8/16.
+ */
+public class TestClass {
+
+    public static void main (String[] args){
+        System.out.println("Hello");
+    }
+
+}
