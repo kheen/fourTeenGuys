d7a22ced948be0804229ca18c5533b234af6db35
Hoang Anh <hthaivu@macalester.edu>
2016-04-08 22:42:33 -0500

    ajkfjasldfj

--- CarlHacks2016/TestModule/src/testPackage/TestClass.java
+++ CarlHacks2016/TestModule/src/testPackage/TestClass.java
@@ -6,7 +6,7 @@ package testPackage;
 public class TestClass {
 
     public static void main (String[] args){
-        System.out.println("My name is Hoang Anh Thai Vu");
+        System.out.println("My name is Hoang Anh Thai Vu ");
     }
 
 }
