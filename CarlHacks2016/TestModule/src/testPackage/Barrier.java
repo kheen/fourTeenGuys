4efa2d91e2d65f4fb04c73769ae0dbbadfcf0a82
themiraclewhipp <cwhipp@macalester.edu>
2016-04-10 01:17:50 -0500

    kdjh

--- CarlHacks2016/TestModule/src/testPackage/Barrier.java
+++ CarlHacks2016/TestModule/src/testPackage/Barrier.java
@@ -1,7 +1,30 @@
 package testPackage;
+import acm.graphics.GRect;
+
+import java.awt.*;
 
 /**
  * Created by Kai on 4/9/16.
  */
-public class Barrier {
+public class Barrier extends GRect{
+
+        private int wall1Y = 100;
+
+        public Barrier(double x, double y, double width, double height) {
+            super(x,y,width,height);
+            Color color = new Color(128,128,128);
+            setColor(color);
+            setFillColor(Color.PINK);
+            setFilled(true);
+
+        }
+
+        public void moveRect (double x, double y){
+        setLocation(x,y);
+
+
+
+
+    }
 }
+
